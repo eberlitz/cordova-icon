ef863339810464e7affd2a92d62a9c2638555467
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-05-02 18:29:04 -0300

    added suport for custom platforms(ionic added)

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var _ = require('underscore');
 var Q = require('q');
 var nodeFs = require('node-fs');
 
+var catalogues = require('./platform-catalogues.js');
+
+var platformIconsCatalogue = catalogues.icons;
+var platformSplashsCatalogue = catalogues.splashs;
+
+var VALID_TRANSFORMATION_METHODS = ['crop', 'resize'];
+
 /**
  * Check which platforms are added to the project and return their icon names and sized
  *
@@ -21,33 +28,7 @@ var getPlatformIcons = function() {
         name: 'ios',
         isAdded: true,
         iconsPath: 'res/icons/ios/',
-        icons: [
-            // Default icon (avoid Apple submit error ITMS-90032)
-            { name: "../../../icon.png", size: 57 },
-            // iOS 8.0+, iPhone 6 Plus
-            { name:'icon-60-3x.png', size:180 },
-            // iOS 7.0+, iPhone / iPod Touch  -->
-            { name:'icon-60.png', size:60 },
-            { name:'icon-60-2x.png', size:120 },
-            // iPad -->
-            { name:'icon-76.png', size:76 },
-            { name:'icon-76-2x.png', size:152 },
-            // iOS 6.1, Spotlight Icon -->
-            { name:'icon-40.png', size:40 },
-            { name:'icon-40-2x.png', size:80 },
-            // iPhone / iPod Touch -->
-            { name:'icon-57.png', size:57 },
-            { name:'icon-57-2x.png', size:114 },
-            // iPad -->
-            { name:'icon-72.png', size:72 },
-            { name:'icon-72-2x.png', size:144 },
-            // iPhone Spotlight and Settings Icon -->
-            { name:'icon-small.png', size:29 },
-            { name:'icon-small-2x.png', size:58 },
-            // iPad Spotlight and Settings Icon -->
-            { name:'icon-50.png', size:50 },
-            { name:'icon-50-2x.png', size:100 }
-        ]
+        icons: platformIconsCatalogue[mainPlatform]['ios']
     });
 
     //ok
@@ -55,14 +36,7 @@ var getPlatformIcons = function() {
         name: 'android',
         iconsPath: 'res/icons/android/',
         isAdded: true,
-        icons: [
-            { name:'icon-36-ldpi.png', size: 36, density:'ldpi' },
-            { name:'icon-48-mdpi.png', size: 48, density:'mdpi' },
-            { name:'icon-72-hdpi.png', size: 72, density:'hdpi' },
-            { name:'icon-96-xhdpi.png', size: 96, density:'xhdpi' },
-            { name:'icon-144-xxhdpi.png', size: 144, density:'xxhdpi' },
-            { name:'icon-192-xxxhdpi.png', size: 192, density:'xxxhdpi' },
-        ]
+        icons: platformIconsCatalogue[mainPlatform]['android']
     });
 
     //ok
@@ -70,10 +44,7 @@ var getPlatformIcons = function() {
         name: 'wp8',
         iconsPath: 'res/icons/wp8/',
         isAdded: true,
-        icons: [
-            { name: 'ApplicationIcon.png', size: 99 },
-            { name: 'Background.png', size: 159 },
-        ]
+        icons: platformIconsCatalogue[mainPlatform]['wp8']
     });
 
     //ok
@@ -81,19 +52,7 @@ var getPlatformIcons = function() {
         name: 'windows',
         iconsPath: 'res/icons/windows/',
         isAdded: true,
-        icons: [
-            { name:'Square150x150Logo.scale-100.png', size:150 },
-            { name:'Square150x150Logo.scale-240.png', size:360 },
-            { name:'Square30x30Logo.scale-100.png', size:30 },
-            { name:'Square310x310Logo.scale-100.png', size:310 },
-            { name:'Square44x44Logo.scale-240.png', size:106 },
-            { name:'Square70x70Logo.scale-100.png', size:70 },
-            { name:'Square71x71Logo.scale-240.png', size:170 },
-            { name:'StoreLogo.scale-100.png', size:50 },
-            { name:'StoreLogo.scale-240.png', size:120 },
-            { name:'Wide310x150Logo.scale-100.png', size:310 },
-            { name:'Wide310x150Logo.scale-240.png', size:744 },
-        ]
+        icons: platformIconsCatalogue[mainPlatform]['windows']
     });
 
     //ok
@@ -101,11 +60,7 @@ var getPlatformIcons = function() {
         name: 'store',
         iconsPath: 'store/icons/',
         isAdded: true,
-        icons: [
-            { path:'./android/', name: "icon-512.png", size: 512 },
-            { path:'./wp8/', name: "icon-300.png", size: 300 },
-            { path:'./ios/', name: "icon-1024.jpg", size: 1024 } // App Store
-        ]
+        icons: platformIconsCatalogue[mainPlatform]['store']
     });
 
     platforms = settings.ICON_PLATFORMS || platforms
@@ -129,18 +84,7 @@ var getPlatformSplashs = function() {
         name: 'ios',
         isAdded: true,
         splashPath: 'res/screens/ios/',
-        splash: [
-            { name:"screen-iphone-portrait.png", width:320, height:480 },           // iPhone
-            { name:"screen-iphone-portrait-2x.png", width:640, height:960 },        // iPhone Retina
-            { name:"screen-ipad-portrait.png", width:768, height:1024 },            // iPad
-            { name:"screen-ipad-portrait-2x.png", width:1536, height:2048 },        // iPad Retina
-            { name:"screen-ipad-landscape.png", width:1024, height:768 },           // iPad
-            { name:"screen-ipad-landscape-2x.png", width:2048, height:1536 },       // iPad Retina
-            { name:"screen-iphone-568h-2x.png", width:640, height:1136 },           // iPhone 5 Retina
-            { name:"screen-iphone-portrait-667h.png", width:750, height:1334 },     // iPhone 6
-            { name:"screen-iphone-portrait-736h.png", width:1242, height:2208 },    // iPhone 6 Plus
-            { name:"screen-iphone-landscape-736h.png", width:2208, height:1242 },   // iPhone 6 Plus
-        ]
+        splash: platformSplashsCatalogue[mainPlatform]['ios']
     });
 
     //ok
@@ -148,16 +92,7 @@ var getPlatformSplashs = function() {
         name: 'android',
         isAdded: true,
         splashPath: 'res/screens/android/',
-        splash: [
-            { name:"screen-ldpi-portrait.png", width: 320, height: 426, density:"port-ldpi" }, // 200x320
-            { name:"screen-ldpi-landscape.png", width: 426, height: 320, density:"land-ldpi" }, // 320x200
-            { name:"screen-hdpi-portrait.png", width: 480, height: 640, density:"port-hdpi" }, // 320x480
-            { name:"screen-hdpi-landscape.png", width: 640, height: 480, density:"land-hdpi" }, // 480x320
-            { name:"screen-mdpi-portrait.png", width: 320, height: 470, density:"port-mdpi" }, // 480x800
-            { name:"screen-mdpi-landscape.png", width: 470, height: 320, density:"land-mdpi" }, // 800x480
-            { name:"screen-xhdpi-portrait.png", width: 720, height: 960, density:"port-xhdpi" }, // 720x1280
-            { name:"screen-xhdpi-landscape.png", width: 960, height: 720, density:"land-xhdpi" } // 1280x720
-        ]
+        splash: platformSplashsCatalogue[mainPlatform]['android']
     });
 
     //ok
@@ -166,12 +101,7 @@ var getPlatformSplashs = function() {
         name: 'wp8',
         isAdded: true,
         splashPath: 'res/screens/wp8/',
-        splash: [
-            { width: 768, height: 1280, name:"SplashScreenImage.jpg" },
-            { width: 480, height: 800, name:"SplashScreenImage.screen-WVGA.jpg" },
-            { width: 768, height: 1280, name:"SplashScreenImage.screen-WXGA.jpg" },
-            { width: 720, height: 1280, name:"SplashScreenImage.screen-720p.jpg" }
-        ]
+        splash: platformSplashsCatalogue[mainPlatform]['wp8']
     });
 
     //ok
@@ -179,20 +109,14 @@ var getPlatformSplashs = function() {
         name: 'windows',
         isAdded: true,
         splashPath: 'res/screens/windows/',
-        splash: [
-            { width:620, height:300, name:"SplashScreen.scale-100.png" },
-            { width:1152, height:1920, name:"SplashScreen.scale-240.png" },
-            { width:1152, height:1920, name:"SplashScreenPhone.scale-240.png" },
-        ]
+        splash: platformSplashsCatalogue[mainPlatform]['windows']
     });
 
     platforms.push({
         name: 'store',
         isAdded: true,
         splashPath: 'store/screens/',
-        splash: [
-            { path: './android/', name: 'cover-store.png', width: 1024, height: 500 },
-        ]
+        splash: platformSplashsCatalogue[mainPlatform]['store']
     });
 
     platforms = settings.SPLASH_PLATFORMS || platforms
@@ -208,6 +132,14 @@ var getPlatformSplashs = function() {
 var settings = {};
 settings.ICON_FILE = path.join('model', 'icon.png');
 settings.SPLASH_FILE = path.join('model', 'splash.png');
+settings.MAIN_PLATFORM = 'phonegap';
+settings.TRANSFORMATION_METHOD = 'crop';
+
+/**
+ * @var {String} current main platform to fetch icons and splash data (currently supports 'phonegap' and 'ionic')
+ */
+
+var mainPlatform = settings.MAIN_PLATFORM;
 
 /**
  * @var {Object} console utils
@@ -226,6 +158,11 @@ display.header = function(str) {
     console.log(' ' + str.cyan.underline);
     console.log('');
 };
+display.info = function(str) {
+    console.log('');
+    console.log(' ' + str.underline);
+    console.log('');
+};
 
 /**
  * Resizes and creates a new icon in the platform's folder.
@@ -237,7 +174,7 @@ display.header = function(str) {
 var generateIcon = function(platform, icon) {
     var deferred = Q.defer();
     try {
-        var filePath = path.join(platform.iconsPath, icon.name);
+        var filePath = path.join(mainPlatform, platform.iconsPath, icon.name);
         var filedirName = path.dirname(filePath);
         if (!fs.existsSync(filedirName)) {
             nodeFs.mkdirSync(filedirName, '0777', true);
@@ -330,6 +267,37 @@ var validIconExists = function() {
 };
 
 
+var validMainPlatform = function(key) {
+    var deferred = Q.defer();
+
+    var catalog = catalogues[key],
+        result  = !!(catalog && !!(catalog[mainPlatform]))
+
+    if(!result) {
+        display.error(mainPlatform + ' is not a valid platform name for ' + key + ' generation');
+    }
+
+    deferred.resolve(result);
+
+    return deferred.promise;
+}
+
+var validSplashPlatform = function() {
+    return validMainPlatform('splashs')
+}
+
+var validIconPlatform = function() {
+    return validMainPlatform('icons')
+}
+
+
+var transformationFunctionName = function(transformationMethod) {
+    var valids = VALID_TRANSFORMATION_METHODS,
+        _func = ((_.includes(valids, transformationMethod) && transformationMethod) || 'crop');
+
+    return _func;
+}
+
 /**
  * Crops and creates a new splash in the platform's folder.
  *
@@ -340,12 +308,15 @@ var validIconExists = function() {
 var generateSplash = function(platform, splash) {
     var deferred = Q.defer();
     try {
-        var filePath = path.join(platform.splashPath, splash.name);
+        var filePath = path.join(mainPlatform, platform.splashPath, splash.name);
         var filedirName = path.dirname(filePath);
         if (!fs.existsSync(filedirName)) {
             nodeFs.mkdirSync(filedirName, '0777', true);
         }
-        ig.crop({
+
+        var _funcToCall = transformationFunctionName(settings.TRANSFORMATION_METHOD);
+
+        var args = [{
             srcPath: settings.SPLASH_FILE,
             dstPath: filePath,
             quality: 1,
@@ -354,15 +325,25 @@ var generateSplash = function(platform, splash) {
             height: splash.height,
         }, function(err, stdout, stderr) {
             if (err) {
+                console.log('ImageMagick ERROR: ' + err);
                 deferred.reject(err);
             } else {
                 deferred.resolve();
                 display.success(splash.name + ' created');
             }
-        });
+        }];
+
+
+        ig[_funcToCall].apply(null, args);
+
     } catch (error) {
+        display.error('Something went wrong...');
+        console.log(error);
+
         deferred.reject(err);
     }
+
+
     return deferred.promise;
 };
 
@@ -398,6 +379,9 @@ var generateSplashes = function(platforms) {
     var deferred = Q.defer();
     var sequence = Q();
     var all = [];
+
+    display.info('Generating splashs screens using ' + settings.TRANSFORMATION_METHOD + ' transformation method');
+
     _(platforms).where({
         isAdded: true
     }).forEach(function(platform) {
@@ -431,11 +415,34 @@ var validSplashExists = function() {
 };
 
 
+
+var validTransformationMethod = function() {
+    var deferred = Q.defer();
+
+    var valids = VALID_TRANSFORMATION_METHODS,
+        isValid = _.includes(valids, settings.TRANSFORMATION_METHOD);
+
+    deferred.resolve(isValid);
+
+    if(!isValid) {
+        display.error(settings.TRANSFORMATION_METHOD + ' is not a valid image transformation method. Try one of those ' + valids);
+    }
+
+    return deferred.promise;
+}
+
+
 function generate(options) {
     settings = options || settings;
+    mainPlatform = settings.MAIN_PLATFORM || 'phonegap';
+
     display.header('Checking Splash & Icon');
-    return Q.all([validIconExists(), validSplashExists()])
+    return Q.all([validTransformationMethod(), validIconPlatform(), validSplashPlatform(), validIconExists(), validSplashExists()])
         .then(function(results) {
+
+            display.header('Generating resources for platform:');
+            display.success(mainPlatform);
+
             var hasIcon = results[0] === true;
             var hasSplash = results[1] === true;
             var promise;
